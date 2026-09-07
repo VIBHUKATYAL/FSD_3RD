e084e2fab2f0d00dcd55f359c5dbb310a3eceb85
VIBHUKATYAL <vibhukatyal75@gmail.com>
2026-09-07 09:32:10 +0530

    async_await

---
+++ javascript/async_await.js
@@ -0,0 +1,29 @@
+function f1(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log("Hi!!!!");
+            resolve();
+        },4000);
+    })
+    
+}
+function f2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("ABES Engineering College");
+            resolve();
+        },2000);
+    })
+}
+
+async function test(){
+    try{
+        await f1();
+        await f2();
+    }
+    catch(err){
+        console.log("ERROR",err);  
+    }
+}
+
+test();
